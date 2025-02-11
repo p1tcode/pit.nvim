b2c007c34fd4dd46189798515c2368b9378f7af0
Pit <perivar@gmail.com>
2025-02-11 19:22:46 +0100

    added termguicolors to options

--- lua/options.lua
+++ lua/options.lua
@@ -66,4 +66,6 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.termguicolors = true
+
 -- vim: ts=2 sts=2 sw=2 et
